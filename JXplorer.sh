6ecaf4d80218f16be380b94f08c8e0d9ca69441a
pawelz <pawelz@pld-linux.org>
2008-07-18 13:59:38 +0000

    - detach from console - cosmetics
    Changed files:
        JXplorer.sh -> 1.2

--- JXplorer.sh
+++ JXplorer.sh
@@ -4,7 +4,8 @@ JX_OPTS=-Djxplorer.config="$HOME/.JXplorer"
 JX_JAVADIR=/usr/share/java/JXplorer
 JX_DATADIR=/usr/share/JXplorer
 
-[ -r /etc/sysconfig/jxplorer ] && . /etc/sysconfig/jxplorer
+# Get config.
+[ -f /etc/sysconfig/jxplorer ] && . /etc/sysconfig/jxplorer
 
 JAVA_BIN="$JAVA_HOME/bin/java"
 
@@ -15,45 +16,16 @@ if ! [ -d "$HOME/.JXplorer" ]; then
   ln -s /etc/JXplorer/jxconfig.txt "$HOME/.JXplorer/jxconfig.txt"
 fi
 
-echo "starting JXplorer..."
-echo
-FAIL=0
 cd "$JX_DATADIR"
 CLASSPATH=`build-classpath junit jhall`
 CLASSPATH="$CLASSPATH:$JX_JAVADIR/jxplorer.jar:$JX_JAVADIR/help.jar"
 export CLASSPATH
-if [ "$1" = "console" ] ; then
-    "$JAVA_BIN" $JX_OPTS com.ca.directory.jxplorer.JXplorer
 
-    if [ "$?" != "0" ]; then
-        FAIL=1
-    fi
-else
-    echo "Use \"$0 console\" if you want logging to the console"
-    "$JAVA_BIN" $JX_OPTS com.ca.directory.jxplorer.JXplorer  >/dev/null 2>&1
-
-    if [ "$?" != "0" ]; then
-        FAIL=1
-    fi
+if [ "$#" == "0" ]; then
+    exec "$JAVA_BIN" $JX_OPTS com.ca.directory.jxplorer.JXplorer < /dev/null > /dev/null 2>&1 &
+  else if [ "$1" = "console" ] ; then
+    exec "$JAVA_BIN" $JX_OPTS com.ca.directory.jxplorer.JXplorer
+  else
+    echo "Usage: $0 [console|help]"
+  fi
 fi
-
-# Check for success
-if [ $FAIL = 0 ]; then
-    exit 0
-fi
-
-cat <<-!
-
-=========================
-JXplorer failed to start
-=========================
-Please ensure that you have appropriate "xhost" access to the machine you are
-running this from. Make sure the DISPLAY environment variable is set correctly.
-Otherwise, ask your Unix Systems Administrator for more information on running
-X Windows applications.
-
-If you require more information run "$0 console" and check the
-error produced.
-!
-
-exit 1
